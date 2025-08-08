22ca37e8b066dcd1108324a8972331d64e5b18ee
faisalahmed3 <faisalahmed4417@gmail.com>
2025-08-08 17:29:02 +0600

    Mongodb Connected

--- Server-Side/index.js
+++ Server-Side/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,8 +10,34 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@faisal.yl6wev4.mongodb.net/?retryWrites=true&w=majority&appName=Faisal`;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("savorlyDB");
+    const recipesCollection = database.collection("recipes");
+
+    // Health check
+    app.get('/', (req, res) => res.send('Savorly Server is getting Hotter.'));
+
+    
+
+  } finally {
+    
+  }
+}
+
+run().catch(console.dir);
 
 // Start server
 app.listen(port, () => {
